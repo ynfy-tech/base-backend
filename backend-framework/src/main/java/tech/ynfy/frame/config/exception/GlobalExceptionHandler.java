216f1aad72941139adcea3fd7baa33e1baef15d1
hsiong <peak1203@126.com>
2022-10-18 19:39:53 +0800

    update GlobalExceptionHandler

--- backend-framework/src/main/java/tech/ynfy/frame/config/exception/GlobalExceptionHandler.java
+++ backend-framework/src/main/java/tech/ynfy/frame/config/exception/GlobalExceptionHandler.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * 全局异常处理器
  * 
- * @author yyx
  */
 @RestControllerAdvice
 public class GlobalExceptionHandler
@@ -20,11 +22,18 @@ public class GlobalExceptionHandler
      * 基础异常
      */
     @ExceptionHandler(Exception.class)
-    public ModelAndView baseException(Exception e)
-    {
-        log.error(e.getMessage(), e);
-        ModelAndView mv = new ModelAndView("error");
-        mv.addObject("exception", e.getMessage());
-        return mv;
+    public void baseException(Exception e, HttpServletResponse response) throws IOException {
+        String msg = "error: " + e.getMessage();
+        log.error(msg, e);
+
+        //返回结果
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        // 获取PrintWriter对象
+        PrintWriter out = response.getWriter();
+        out.print(msg);
+        // 释放PrintWriter对象
+        out.flush();
+        out.close();
     }
 }
